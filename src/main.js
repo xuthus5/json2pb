124765efaf6a25eaf8c2f1ad036cb0af5672edad
xuye <xuye@pinquest.cn>
2020-08-25 16:52:15 +0800

    fix n

--- src/main.js
+++ src/main.js
@@ -6,12 +6,23 @@ import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
 
 import VueClipboard from 'vue-clipboard2'
 
+import VueHighlightJS from 'vue-highlight.js';
+
+// Highlight.js languages (Only required languages)
+import protobuf from 'highlight.js/lib/languages/protobuf';
+
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'highlight.js/styles/default.css';
 
 Vue.use(BootstrapVue)
 Vue.use(BootstrapVueIcons)
 Vue.use(VueClipboard)
+Vue.use(VueHighlightJS, {
+	languages: {
+		protobuf
+	}
+});
 
 Vue.config.productionTip = false
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,87 +1,106 @@
 <template>
-    <div class="home">
-        <table>
-            <tbody>
-            <tr>
-                <th style="width: 45%;">JSON</th>
-                <th style="width: 10%; font-size: 22px;"> --></th>
-                <th style="width: 45%;">Protobuf Message
-                    <button class="copy btn btn-grey" type="button"
-                            v-clipboard:copy="rawMess"
-                            v-clipboard:success="onCopy"
-                            v-clipboard:error="onError"
-                    >Copy
-                    </button>
-                </th>
-            </tr>
-            </tbody>
-        </table>
-        <table>
-            <tbody>
-            <tr>
-                <td style="width: 50%;">
-                    <textarea class="form-control mb-2" v-model="rawJson" placeholder="Paste JSON here"
-                              rows="35"></textarea>
-                </td>
-                <td style="width: 50%;">
-                    <textarea class="form-control mb-2" readonly v-model="rawMess"
-                              placeholder="Protobuf Message will appear here" rows="35"></textarea>
-                    <!--                    <div id="output"><span style="color: #777;">Go will appear here</span></div>-->
-                </td>
-            </tr>
-            </tbody>
-        </table>
+  <div class="home">
+    <table>
+      <tbody>
+        <tr>
+          <th style="width: 45%;">JSON</th>
+          <th style="width: 10%; font-size: 22px;">--></th>
+          <th style="width: 45%;">
+            Protobuf Message
+            <button
+              class="copy btn btn-grey"
+              type="button"
+              v-clipboard:copy="rawMess"
+              v-clipboard:success="onCopy"
+              v-clipboard:error="onError"
+            >Copy</button>
+          </th>
+        </tr>
+      </tbody>
+    </table>
+    <table>
+      <tbody>
+        <tr>
+          <td style="width: 50%;">
+            <textarea
+              class="form-control mb-2"
+              v-model="rawJson"
+              placeholder="Paste JSON here"
+              rows="25"
+            ></textarea>
+          </td>
+          <td style="width: 50%;">
+            <span v-if="rawMess === ''">
+              <b-card class="output" bg-variant="light" text-variant="dark">
+                <b-card-text>Protobuf Message will appear here.</b-card-text>
+              </b-card>
+            </span>
+            <span v-else>
+              <b-card class="output" bg-variant="light" text-variant="dark">
+                <b-card-text class="text-left">
+                  <highlight-code lang="protobuf">{{rawMess}}</highlight-code>
+                </b-card-text>
+              </b-card>
+            </span>
+          </td>
+        </tr>
+      </tbody>
+    </table>
 
-        <button type="button" @click="trans" class="btn btn-primary">Convert</button>
+    <button type="button" @click="trans" class="btn btn-primary">Convert</button>
 
-        <footer class="mt-5">
-            <b-container>
-                <b-row>
-                    <b-col md="6" lg="6" xl="6" offset="3">
-                        <p>
-                            This tool instantly converts JSON into a Protobuf Message definition. Paste a JSON structure
-                            on the left and the equivalent Protobuf Message will be generated to the right, which you
-                            can paste into your program. The script has to make some assumptions, so double-check the
-                            output!
-                        </p>
-                        <p>
-                            Try converting JSON from the Example <a href="javascript:" @click="example">DataSet</a>.
-                        </p>
-                        <p>
-                            © 2020 <a href="https://xuthus.cc" target="_blank">Young Xu</a>
-                            • <a href="https://github.com/xuthus5/json2pb" target="_blank">View on GitHub</a>
-                        </p>
-                    </b-col>
-                </b-row>
-            </b-container>
-
-            © 2020 Young Xu
-            <br>
-            You can find me on <a href="https://github.com/xuthus5" target="_blank">GitHub</a>.
-        </footer>
-    </div>
+    <footer class="mt-5 mb-3">
+      <b-container>
+        <b-row>
+          <b-col md="6" lg="6" xl="6" offset="3">
+            <p>
+              This tool instantly converts JSON into a Protobuf Message definition. Paste a JSON structure
+              on the left and the equivalent Protobuf Message will be generated to the right, which you
+              can paste into your program. The script has to make some assumptions, so double-check the
+              output!
+            </p>
+            <p>
+              Try converting JSON from the Example
+              <a href="javascript:" @click="example">DataSet</a>.
+            </p>
+            <p>
+              © 2020
+              <a href="https://xuthus.cc" target="_blank">Young Xu</a>
+              •
+              <a
+                href="https://github.com/xuthus5/json2pb"
+                target="_blank"
+              >View on GitHub</a>
+            </p>
+          </b-col>
+        </b-row>
+      </b-container>© 2020 Young Xu
+      <br />You can find me on
+      <a href="https://github.com/xuthus5" target="_blank">GitHub</a>.
+    </footer>
+  </div>
 </template>
 
 <script>
-String.prototype.times = function(n) {
-    if ( n == 1 ) {
-        return this;
-    }
-    var midRes = this.times(Math.floor(n/2));
-    midRes += midRes;
-    if ( n % 2 ) {
-        midRes += this;
-    }
-    return midRes;
-}
+String.prototype.times = function (n) {
+  if (n == 1) {
+    return this;
+  }
+  var midRes = this.times(Math.floor(n / 2));
+  midRes += midRes;
+  if (n % 2) {
+    midRes += this;
+  }
+  return midRes;
+};
 
 export default {
-    name: "Home",
-    data() {
-        return {
-            rawJson: "",
-            rawMess: "",
-            expJson: `[
+  name: "Home",
+  data() {
+    return {
+      rawJson: "",
+      rawMess: "",
+      expJson: `[
 \t{
 \t\t"input_index": 0,
 \t\t"candidate_index": 0,
@@ -169,123 +188,157 @@ export default {
 \t\t}
 \t}
 ]`,
-        };
+    };
+  },
+  methods: {
+    example() {
+      this.rawJson = this.expJson;
     },
-    methods: {
-        example() {
-            this.rawJson = this.expJson;
-        },
-        onCopy(e) {
-            console.log('success');
-        },
-        onError(e) {
-            console.log('copy error:', e);
-        },
-        trans() {
-            let checkJson = this.isJSON(this.rawJson);
-            if (!checkJson) {
-                alert("It is not a json string!");
-                return;
-            }
+    onCopy(e) {
+      console.log("success");
+    },
+    onError(e) {
+      console.log("copy error:", e);
+    },
+    trans() {
+      let checkJson = this.isJSON(this.rawJson);
+      if (!checkJson) {
+        alert("It is not a json string!");
+        return;
+      }
 
-            //将json转为protobuf message
-            this.rawMess = this.genTpl(JSON.parse(this.rawJson));
-        },
-        isJSON(str) {
-            if (typeof str == "string") {
-                try {
-                    JSON.parse(str);
-                    return true;
-                } catch (e) {
-                    console.log(e);
-                    return false;
-                }
-            }
-            alert("It is not a string!");
-        },
-        genTpl(obj) {
-            return `message AutoGenerate {
+      //convert json to protobuf message
+      this.rawMess = this.genTpl(JSON.parse(this.rawJson));
+    },
+    isJSON(str) {
+      if (typeof str == "string") {
+        try {
+          JSON.parse(str);
+          return true;
+        } catch (e) {
+          console.log(e);
+          return false;
+        }
+      }
+      alert("It is not a string!");
+    },
+    genTpl(obj) {
+      return `message AutoGenerate {
 ${this.parseObj(obj,1)}
 }`;
-        },
-        parseObj(obj,level) {
-            let index = 1;
-            let body = "";
-            for (let key in obj) {
-                let typ = typeof obj[key];
-                switch (typ) {
-                    case "number":
-                        body += ('\t'.times(level)) + "int64 " + key + " = " + index + ";\n";
-                        break;
-                    case "boolean":
-                        body += ('\t'.times(level)) + "bool " + key + " = " + index + ";\n";
-                        break;
-                    case "string":
-                        body += ('\t'.times(level)) + "string " + key + " = " + index + ";\n";
-                        break;
-                    case "object":
-                        if (obj[key] instanceof Array) {
-                            //是数组
-                            //数组里面是基本类型?
-                            let item = obj[key];
-                            let typ = typeof item[0];
-                            if (typ !== "object") {
-                                body += ('\t'.times(level)) + "repeated " + this.getBaseType(typ) +" " +  key + " = " + index + ";\n";
-                            } else {
-                                let newKey = key.slice(0, 1).toUpperCase() + key.slice(1);
-                                body += ('\t'.times(level)) + `message ${newKey} {
-${this.parseObj(item[0],level+1)}
-${'\t'.times(level) }}
-${'\t'.times(level) }repeated ${newKey} ${key} = ${index};
-`;
-                            }
-                        } else {
-                            //是对象
-                            //when parent node is array ; then set default message name: data_index
-                            let realMessageKey = key;
-                            if (key == 0) realMessageKey = 'Data_' + key;
-
-                            let item = obj[key];
-                            realMessageKey = realMessageKey.slice(0, 1).toUpperCase() + realMessageKey.slice(1);
-                            let realDataKey = realMessageKey.slice(0, 1).toLowerCase() + realMessageKey.slice(1);
-                            body += ('\t'.times(level)) + `message ${realMessageKey} {
-${this.parseObj(item,level+1)}
-${'\t'.times(level) }}
-${'\t'.times(level) }repeated ${realMessageKey} ${realDataKey} = ${index};
-`;
-                        }
-                        break;
-                }
-                index++;
-                if (obj instanceof Array) {
-                    break;
-                }
-            }
-            return body;
-        },
-        getBaseType(typ) {
-            switch (typ) {
-                case "number":
-                    return "int64";
-                case "boolean":
-                    return "bool";
-                case "string":
-                    return "string";
-                default:
-                    return "undefined";
-            }
-        },
     },
+    parseObj(obj, level) {
+      let index = 1;
+      let body = "";
+      for (let key in obj) {
+        let typ = typeof obj[key];
+        switch (typ) {
+          case "number":
+            body += "\t".times(level) + "int64 " + key + " = " + index + ";\n";
+            break;
+          case "boolean":
+            body += "\t".times(level) + "bool " + key + " = " + index + ";\n";
+            break;
+          case "string":
+            body += "\t".times(level) + "string " + key + " = " + index + ";\n";
+            break;
+          case "object":
+            if (obj[key] instanceof Array) {
+              //isArray
+              //base type in array item
+              let item = obj[key];
+              let typ = typeof item[0];
+              if (typ !== "object") {
+                body +=
+                  "\t".times(level) +
+                  "repeated " +
+                  this.getBaseType(typ) +
+                  " " +
+                  key +
+                  " = " +
+                  index +
+                  ";\n";
+              } else {
+                let newKey = key.slice(0, 1).toUpperCase() + key.slice(1);
+                body +=
+                  "\t".times(level) +
+                  `message ${newKey} {
+${this.parseObj(item[0], level + 1)}
+${"\t".times(level)}}
+${"\t".times(level)}repeated ${newKey} ${key} = ${index};
+`;
+              }
+            } else {
+              //isObject
+              //when parent node is array ; then set default message name: data_index
+              let realMessageKey = key;
+              if (key == 0) realMessageKey = "Data_" + key;
+
+              let item = obj[key];
+              realMessageKey =
+                realMessageKey.slice(0, 1).toUpperCase() +
+                realMessageKey.slice(1);
+              let realDataKey =
+                realMessageKey.slice(0, 1).toLowerCase() +
+                realMessageKey.slice(1);
+              body +=
+                "\t".times(level) +
+                `message ${realMessageKey} {
+${this.parseObj(item, level + 1)}
+${"\t".times(level)}}
+${"\t".times(level)}repeated ${realMessageKey} ${realDataKey} = ${index};
+`;
+            }
+            break;
+        }
+        index++;
+        if (obj instanceof Array) {
+          break;
+        }
+      }
+
+      return body.substring(0,body.lastIndexOf('\n'))
+    },
+    getBaseType(typ) {
+      switch (typ) {
+        case "number":
+          return "int64";
+        case "boolean":
+          return "bool";
+        case "string":
+          return "string";
+        default:
+          return "undefined";
+      }
+    },
+  },
 };
 </script>
 
 <style scoped>
-
 .copy {
-    color: #FFFFFF;
+  color: #ffffff;
 }
 
 .btn-grey {
-    background: #80808099;
+  background: #80808099;
+}
+
+.output {
+  background-color: #e9ecef;
+  white-space: pre;
+  width: 100%;
+  height: 614px;
+  overflow: scroll;
+  tab-size: 4;
+}
+.card-body {
+  padding: 0;
+}
+.hljs {
+  padding: 1.5em;
+}
+pre {
+  margin-bottom: 0;
 }
 </style>
